b416bc59ed71f297d52232de79b24277342f9d50
AlessandroGranato <a.granato.89@gmail.com>
2024-01-11 18:26:58 +0100

    Feature - 002 - handle error in AuthFilter - add error handling in AuthFilter class, both for Missing header or Auth verification - add routes for new auth services and for bugo-data-shell services - add common-bom in dependencyManagement in main pom - add common-dto and common-error dependencies in application main

--- application/src/main/java/com/pyrosandro/gtw/config/RouteConfig.java
+++ application/src/main/java/com/pyrosandro/gtw/config/RouteConfig.java
@@ -18,24 +18,53 @@ public class RouteConfig {
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
+                //AUTH ROUTES
+                .route(r -> r.path("/auth/api/auth/signup").uri("lb://AUTH-SERVICE"))
+                .route(r -> r.path("/auth/api/auth/signin").uri("lb://AUTH-SERVICE"))
+                .route(r -> r.path("/auth/api/test/all").uri("lb://AUTH-SERVICE"))
+                .route(r -> r.path("/auth/api/test/user")
+                        .filters(f -> f.filter(authFilter))
+                        .uri("lb://AUTH-SERVICE"))
                 .route(r -> r.path("/auth/api/test/mod")
-                        .filters(f ->
-                                f.filter(authFilter)
-                                        .rewritePath("/auth/api/test/(?<param>.*)", "/auth/api/test/${param}")
-                        )
+                        .filters(f -> f.filter(authFilter))
                         .uri("lb://AUTH-SERVICE"))
-                .route(r -> r.path("/auth/api/auth/{param1}/test/{param2}")
-                        .filters(f ->
-                                f.filter(authFilter)
-                                        .rewritePath("/auth/api/auth/(?<param1>.*)/test/(?<param2>.*)", "/auth/api/auth/${param1}/test/${param2}")
-                        )
+                .route(r -> r.path("/auth/api/test/admin")
+                        .filters(f -> f.filter(authFilter))
                         .uri("lb://AUTH-SERVICE"))
-                .route(r -> r.path("/auth/api/test/{param}")
-                        .filters(f ->
-                                f.filter(authFilter)
-                                        .rewritePath("/auth/api/test/(?<param>.*)", "/auth/api/test/${param}")
-                        )
+                .route(r -> r.path("/auth/api/auth/refresh-token").uri("lb://AUTH-SERVICE"))
+                .route(r -> r.path("/auth/api/auth/authorize-resource")
+                        .filters(f -> f.filter(authFilter))
                         .uri("lb://AUTH-SERVICE"))
+                .route(r -> r.path("/auth/api/auth/{id}")
+                        .filters(f -> f.filter(authFilter))
+                        .uri("lb://AUTH-SERVICE"))
+
+                //BUGO-DATA-SHELL ROUTES
+                .route(r -> r.path("/bds/api/devices/{id}")
+                        .filters(f -> f.filter(authFilter))
+                        .uri("lb://BDS-SERVICE"))
+                .route(r -> r.path("/bds/api/devices")
+                        .filters(f -> f.filter(authFilter))
+                        .uri("lb://BDS-SERVICE"))
+                .route(r -> r.path("/bds/api/users/{id}")
+                        .filters(f -> f.filter(authFilter))
+                        .uri("lb://BDS-SERVICE"))
+                .route(r -> r.path("/bds/api/users")
+                        .filters(f -> f.filter(authFilter))
+                        .uri("lb://BDS-SERVICE"))
+                .route(r -> r.path("/bds/api/temperatures/{id}")
+                        .filters(f -> f.filter(authFilter))
+                        .uri("lb://BDS-SERVICE"))
+                .route(r -> r.path("/bds/api/temperatures")
+                        .filters(f -> f.filter(authFilter))
+                        .uri("lb://BDS-SERVICE"))
+//                  Used to keep track of rewrite param. Should not be useful anymore
+//                .route(r -> r.path("/auth/api/auth/{param1}/test/{param2}")
+//                        .filters(f ->
+//                                f.filter(authFilter)
+//                                        .rewritePath("/auth/api/auth/(?<param1>.*)/test/(?<param2>.*)", "/auth/api/auth/${param1}/test/${param2}")
+//                        )
+//                        .uri("lb://AUTH-SERVICE"))
                 .build();
     }
 }
